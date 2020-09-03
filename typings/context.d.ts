070a053c0a1de2d434fb56e5c6ae108be27a1ee6
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2020-09-03 14:00:25 -0400

    chore: remove superflous connect

--- typings/context.d.ts
+++ typings/context.d.ts
@@ -7,13 +7,3 @@ type ContextValue<S, T extends string = string, P = unknown> = {
     reducer: Reducer<S>;
     state: S;
 };
-
-type MapStateToProps<S, R = unknown, U = R> = (
-    state: S,
-    ownProps?: React.PropsWithChildren<R>,
-) => React.PropsWithChildren<U>;
-
-type MapDispatchToProps<D, R = unknown, U = R> = (
-    dispatch: D,
-    ownProps?: React.PropsWithChildren<R>,
-) => React.PropsWithChildren<U>;
