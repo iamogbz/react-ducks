9041439b67f9811bb3ff450767ef6fe635ba2016
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2020-10-14 00:30:47 -0400

    fix: remove unused useOnce hook

--- src/hooks/useOnce.ts
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-
-export function useOnce(callback: React.EffectCallback): void {
-    const isFirstRun = React.useRef(true);
-    if (!isFirstRun.current) return;
-    callback();
-    isFirstRun.current = false;
-}
