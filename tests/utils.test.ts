d8912bb669b7ac51684e6f397654253de25c695c
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2020-11-10 06:37:48 -0500

    feat: remove connect api

--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -1,51 +1,16 @@
 import { createAction } from "src";
-import { bindActionCreators } from "src/utils/bindActionCreators";
+import { bindActionCreator } from "src/utils/bindActionCreators";
 
 describe("bindActionCreators", () => {
     it("binds action creator function to dispatch", () => {
         const dispatch = jest.fn();
         const action = { payload: "Some payload", type: "ACTION_TYPE" };
-        const boundActionCreator = bindActionCreators(
+        const boundActionCreator = bindActionCreator(
             createAction(action.type, () => action),
             dispatch,
         );
-        expect(boundActionCreator()).toBeUndefined();
+        expect(boundActionCreator!()).toBeUndefined();
         expect(dispatch).toHaveBeenCalledTimes(1);
         expect(dispatch).toHaveBeenCalledWith(action);
     });
-
-    it("binds action creator mapping to dispatch", () => {
-        const dispatch = jest.fn();
-        const action = { payload: "Some payload", type: "ACTION_TYPE" };
-        const boundActionCreators = bindActionCreators(
-            ({
-                action1: () => action,
-                action2: null,
-                action3: undefined,
-                action4: "non function",
-            } as unknown) as ActionCreatorMapping,
-            dispatch,
-        );
-        expect(boundActionCreators.action1()).toBeUndefined();
-        expect(dispatch).toHaveBeenCalledTimes(1);
-        expect(dispatch).toHaveBeenCalledWith(action);
-        expect(boundActionCreators.action2).toBeUndefined();
-        expect(boundActionCreators.action3).toBeUndefined();
-        expect(boundActionCreators.action4).toBeUndefined();
-    });
-
-    it.each`
-        actionCreator
-        ${"non function"}
-        ${null}
-        ${undefined}
-    `(
-        "does not binds unsupported action creator to dispatch",
-        ({ actionCreator }) => {
-            const dispatch = jest.fn();
-            expect(() => bindActionCreators(actionCreator, dispatch)).toThrow(
-                "bindActionCreators expected an object or a function",
-            );
-        },
-    );
 });
