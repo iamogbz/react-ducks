a1efa9a079b9dbefb08bf3798093de6e40846761
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2020-11-11 21:26:03 -0500

    fix: useSelector deps should only update on new arguments

--- src/hooks/useSelector.ts
+++ src/hooks/useSelector.ts
@@ -2,15 +2,16 @@ import * as React from "react";
 import { GlobalContext } from "..";
 
 export function useSelector<S, R, E extends unknown[], T extends string, P>(
-    selector?: Selector<S, R, E, T, P>,
+    selector: Selector<S, R, E, T, P>,
     Context?: Context<S, T, P>,
     ...args: E
 ): Nullable<R> {
     const { state } = React.useContext(Context ?? GlobalContext);
     return React.useMemo(
         function select() {
-            return selector?.(state, ...args);
+            return selector(state, ...args);
         },
-        [args, selector, state],
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        [selector, state, ...args],
     );
 }
