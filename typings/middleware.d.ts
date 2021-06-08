d46ba646adc5e1397ba181b521e23f0808c085c2
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2021-06-08 19:25:43 -0400

    style: fix prettier lint

--- typings/middleware.d.ts
+++ typings/middleware.d.ts
@@ -6,5 +6,7 @@ interface MiddlewareAPI<
     getState: () => State;
 }
 
-type Middleware<State = unknown, D extends ContextDispatch = ContextDispatch> =
-    (api: MiddlewareAPI<State, D>) => (next: D) => D;
+type Middleware<
+    State = unknown,
+    D extends ContextDispatch = ContextDispatch,
+> = (api: MiddlewareAPI<State, D>) => (next: D) => D;
